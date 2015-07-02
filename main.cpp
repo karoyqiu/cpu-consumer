8099372f02f1134e270e3e05b53b63c5425c6db8
Qiu Daolin <qiudaolin@zhongfu.net>
2015-07-02 11:35:18 +0800

    Add a Boost version of eating CPUs.

--- main.cpp
+++ main.cpp
@@ -13,26 +13,50 @@
 #include <iostream>
 
 #if defined(HAVE_BOOST)
+#include <boost/thread.hpp>
 #elif defined(HAVE_OPENMP)
 #include <omp.h>
+#else
+#error "You must have Boost 1.56+ or OpenMP."
 #endif
+
+
+static void prompt_eating(int index)
+{
+    std::cout << "[" << index << "]: Eating CPU..." << std::endl;
+}
+
+
+static void eat_cpu()
+{
+    for (int i = 0; ; i++);
+}
 
 
 int main(int argc, char *argv[])
 {
 #if defined(HAVE_BOOST)
+
+    boost::thread_group tg;
+
+    for (unsigned i = 0; i < boost::thread::hardware_concurrency(); i++)
+    {
+        prompt_eating(i);
+        tg.create_thread(eat_cpu);
+    }
+
+    tg.join_all();
+
 #elif defined(HAVE_OPENMP)
+
 #pragma omp parallel
 {
-#pragma omp critical
-{
-    std::cout << "[" << omp_get_thread_num() << "]: Eating CPU..." << std::endl;
+    #pragma omp critical
+    prompt_eating(omp_get_thread_num());
+
+    eat_cpu();
 }
 
-    for (int i = 0;; i++)
-    {
-    }
-}
 #endif
 
     return 0;
