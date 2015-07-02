6655ec4273adc1ded075d18e9ed93ad151f71fca
Qiu Daolin <qiudaolin@zhongfu.net>
2015-07-02 10:41:35 +0800

    Add a OMP version of consumer.

---
+++ main.cpp
@@ -0,0 +1,39 @@
+﻿/*! ***********************************************************************************************
+ *
+ * \file        main.cpp
+ * \brief       The main file of CPU consumer.
+ *
+ * \version     0.1
+ * \date        2015/07/02
+ *
+ * \author      Roy QIU (karoyqiu@gmail.com)
+ * \copyright   © Roy QIU. MIT license.
+ *
+ **************************************************************************************************/
+#include <iostream>
+
+#if defined(HAVE_BOOST)
+#elif defined(HAVE_OPENMP)
+#include <omp.h>
+#endif
+
+
+int main(int argc, char *argv[])
+{
+#if defined(HAVE_BOOST)
+#elif defined(HAVE_OPENMP)
+#pragma omp parallel
+{
+#pragma omp critical
+{
+    std::cout << "[" << omp_get_thread_num() << "]: Eating CPU..." << std::endl;
+}
+
+    for (int i = 0;; i++)
+    {
+    }
+}
+#endif
+
+    return 0;
+}
